5af9fd8e226a3cc4b8f587ac88ba952387d66ae7
Ratheesh1986 <76087168+Ratheesh1986@users.noreply.github.com>
2022-06-07 17:06:53 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("HI Gus newbie to learn docker");
+    System.out.println("HI Guy's newbie to learn docker");
   }
 }
